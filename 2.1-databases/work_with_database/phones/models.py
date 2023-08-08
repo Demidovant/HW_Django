0764e18b42e8c00e630ba281b61b61337fafb0c9
Anton Demidov <anton_demidov@list.ru>
2023-08-08 23:25:27 +0300

    Change models, settings, views

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,10 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    price = models.PositiveBigIntegerField(null=False)
+    image = models.URLField(null=False)
+    release_date = models.DateField(null=False)
+    lte_exists = models.BooleanField(null=False)
+    slug = models.SlugField(null=False)
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from phones.models import Phone
 
 
 def index(request):
@@ -6,12 +7,21 @@ def index(request):
 
 
 def show_catalog(request):
+    sort_type = request.GET.get("sort", None)
+    sort_types = {
+        'name': Phone.objects.order_by('name'),
+        'min_price': Phone.objects.order_by('price'),
+        'max_price': Phone.objects.order_by('-price'),
+        'release_date': Phone.objects.order_by('-release_date'),
+        None: Phone.objects.all(),
+    }
+    context = {"phones": sort_types[sort_type]}
     template = 'catalog.html'
-    context = {}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.filter(slug=slug)[0]
+    context = {"phone": phone}
     return render(request, template, context)
