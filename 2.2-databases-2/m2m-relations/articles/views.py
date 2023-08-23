5acada45a31d0413492b9594d62f82baf59c0908
Anton Demidov <anton_demidov@list.ru>
2023-08-23 23:46:15 +0300

    Change views.py

--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -1,14 +1,36 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
-from articles.models import Article
+from articles.models import Article, Tag
 
 
 def articles_list(request):
+    """Функция формирует сгруппированный по дате публикации список статей и возвращает рендер"""
     template = 'articles/news.html'
-    context = {}
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = '-published_at'
-
+    # ordering = '-published_at'
+    articles = Article.objects.all()  # order_by(ordering)
+    context = {'object_list': articles}
     return render(request, template, context)
+
+
+def add_tags(request):
+    """Функция добавляет список тэгов в БД и возвращает уведомление."""
+    tag_list = [
+        'Наука',
+        'Здоровье',
+        'Культура',
+        'Город',
+        'Космос',
+        'Международные отношения',
+        'Музыка'
+    ]
+    tag_str = []
+    errors_str = []
+    for tag in tag_list:
+        try:
+            Tag.objects.create(name=tag)
+            tag_str.append(tag)
+        except:
+            errors_str.append(tag)
+    result = f'Успешно добавлен в БД следующий список: {", ".join(tag_str)}.<br>'\
+             f'Уже были ранее добавлены в БД: {", ".join(errors_str)}.'
+    return HttpResponse(result)
