fbf632ec729426e819c3b97c773daca45f185f9d
Anton Demidov <anton_demidov@list.ru>
2023-08-23 23:45:42 +0300

    Change admin.py

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,32 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+from .models import Article, Scope
 
-from .models import Article
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        main_tag_counter = 0
+        tags_all = (list(map(lambda x: x.cleaned_data.get('tag'), self.forms)))
+        for form in self.forms:
+            if form.cleaned_data.get('is_main'):  # исправлено обращение по ключу. использован get
+                main_tag_counter += 1
+            tag_current = form.cleaned_data.get('tag')
+            if tags_all.count(tag_current) > 1:
+                raise ValidationError(f'Должен быть выбран один Тэг/раздел: {tag_current}')
+        if main_tag_counter < 1:
+            raise ValidationError('Должен быть выбран основной Тэг/раздел')
+        if main_tag_counter > 1:
+            raise ValidationError('Основной Тэг/раздел должен быть один')
+        return super().clean()
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+    extra = 1
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ScopeInline]
