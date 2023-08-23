877b68312f3e6b16bb75e5a70fcc0815d3b3f642
Anton Demidov <anton_demidov@list.ru>
2023-08-23 23:45:58 +0300

    Change settings.py

--- 2.2-databases-2/m2m-relations/website/settings.py
+++ 2.2-databases-2/m2m-relations/website/settings.py
@@ -78,7 +78,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_m2m_relations',
         'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'PORT': '5433',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
     }
 }
 
