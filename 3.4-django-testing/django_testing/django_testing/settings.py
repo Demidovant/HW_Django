65d5c35542ad90d99fd03ec1a05933392eb240b7
Anton Demidov <anton_demidov@list.ru>
2023-09-18 01:23:43 +0300

    Update serializers.py, settings.py, test_courses_api.py

--- 3.4-django-testing/django_testing/django_testing/settings.py
+++ 3.4-django-testing/django_testing/django_testing/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'django_filters',
-
+    'rest_framework.authtoken',
     'students',
 ]
 
@@ -82,7 +82,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_django_testing',
         'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'PORT': '5433',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres'
     }
 }
 
@@ -124,3 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
+}
+
+MAX_STUDENTS_PER_COURSE = 20
--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -1,2 +1,150 @@
-def test_example():
-    assert False, "Just test example"
+import pytest
+from model_bakery import baker
+from rest_framework.authtoken.admin import User
+from rest_framework.test import APIClient
+from students.models import Course, Student
+from django.urls import reverse
+import random
+from django_testing import settings
+
+
+@pytest.fixture
+def client():
+    return APIClient()
+
+
+@pytest.fixture
+def user():
+    return User.objects.create_user('testuser')
+
+
+@pytest.fixture
+def course_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Course, *args, **kwargs)
+
+    return factory
+
+
+@pytest.fixture
+def student_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Student, *args, **kwargs)
+
+    return factory
+
+
+@pytest.mark.django_db
+def test_get_courses(user, client, course_factory):
+    courses = course_factory(_quantity=10)
+    url = reverse("courses-list")
+    response = client.get(url)
+    assert response.status_code == 200
+    response_data = response.json()
+    assert len(response_data) == len(courses)
+
+
+@pytest.mark.django_db
+def test_get_one_course(user, client, course_factory):
+    courses = course_factory(_quantity=10)
+    index = random.randint(0, 9)
+    course_id = courses[index].pk
+    course_name = courses[index].name
+    url = reverse('courses-detail', args=[course_id])
+    response = client.get(url)
+    assert response.status_code == 200
+    response_data = response.json()
+    assert response_data['name'] == course_name
+
+
+@pytest.mark.django_db
+def test_get_filter_courses_by_id(user, client, course_factory):
+    courses = course_factory(_quantity=10)
+    index = random.randint(0, 9)
+    course_id = courses[index].pk
+    course_name = courses[index].name
+    url = reverse("courses-list")
+    filter_data = {'id': course_id}
+    response = client.get(url, filter_data)
+    assert response.status_code == 200
+    response_data = response.json()
+    assert response_data[0]['name'] == course_name
+
+
+@pytest.mark.django_db
+def test_get_filter_courses_by_name(user, client, course_factory):
+    courses = course_factory(_quantity=10)
+    index = random.randint(0, 9)
+    course_id = courses[index].pk
+    course_name = courses[index].name
+    url = reverse("courses-list")
+    filter_data = {'name': course_name}
+    response = client.get(url, filter_data)
+    assert response.status_code == 200
+    response_data = response.json()
+    assert response_data[0]['id'] == course_id
+
+
+@pytest.mark.django_db
+def test_create_course(user, client):
+    count = Course.objects.count()
+    url = reverse("courses-list")
+    data = {'name': 'test_course'}
+    response = client.post(url, data=data)
+    assert response.status_code == 201
+    assert Course.objects.count() == count + 1
+    response_data = response.json()
+    assert response_data['name'] == data['name']
+
+
+@pytest.mark.django_db
+def test_update_course(user, client, course_factory):
+    courses = course_factory(_quantity=10)
+    index = random.randint(0, 9)
+    course_id = courses[index].pk
+    update_data = {'pk': course_id, 'name': 'test_course_update_name'}
+    url = reverse("courses-detail", args=[course_id])
+    response = client.patch(url, data=update_data)
+    assert response.status_code == 200
+    response_data = response.json()
+    assert response_data['name'] == update_data['name']
+
+
+@pytest.mark.django_db
+def test_delete_course(user, client, course_factory):
+    courses = course_factory(_quantity=10)
+    index = random.randint(0, 9)
+    course_id = courses[index].pk
+    url = reverse("courses-detail", args=[course_id])
+    response = client.delete(url)
+    assert response.status_code == 204
+    response_after_delete = client.get(url)
+    assert response_after_delete.status_code == 404
+
+
+@pytest.fixture
+def students_quantity():
+    return settings.MAX_STUDENTS_PER_COURSE
+
+
+@pytest.mark.parametrize(
+    ['students_count', 'expected_status'],
+    (
+            (20, 201),
+            (21, 400),
+            (1000, 400),
+            (10, 201),
+            (1, 201),
+    )
+)
+@pytest.mark.django_db
+def test_students_quantity(client, students_count, expected_status, student_factory):
+    student = student_factory(_quantity=1)
+    students_list = [student[0].pk] * students_count
+    url = reverse('courses-list')
+    data = {
+        'name': 'test_course',
+        'students': students_list
+    }
+    response = client.post(url, data=data)
+    assert response.status_code == expected_status
